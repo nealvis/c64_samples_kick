d043e583a49c2e7d666580038e9a6d85bd063176
Neal Smith <nealvis@gmail.com>
2021-05-28 17:47:27 -0700

    cleaned up code, using .const etc.

--- screen_colors/screencolors.asm
+++ screen_colors/screencolors.asm
@@ -1,6 +1,5 @@
 // 10 SYS (4096)
-
-*=$0801 "BASIC"
+*=$0801 "BASIC Start"
 
         // These bytes are a one line basic program that will 
         // do a sys call to assembly language portion of
@@ -13,43 +12,39 @@
         .byte $20, $28,  $34, $30, $39, $36, $29 // ASCII for " (4096)"
         .byte $00, $00, $00      // end of basic program (addr $080E from above)
 
-
-.var CLEAR_SCREEN_KERNAL = $E544     // Kernal routine to clear screen
-
-.var PRINT_STRING_BASIC = $AB1E      // Basic routine to print text
-
-// inner loop index/counter
+// program var for inner loop index/counter
 inner_counter: .byte 0
 
-// outer loop index/counter
+// program var for outer loop index/counter
 outer_counter: .byte 0
 
 
-*=$1000 "Main"
-.var border_color_addr = $D020
-.var background_color_addr = $D021
-
-      
+*=$1000 "Main Start"
 Main:
-        jsr CLEAR_SCREEN_KERNAL // clear screeen leave cursor upper left
-        //jsr PrintHello
+        // assembler constants for special addresses
+        .const BORDER_COLOR_ADDR = $D020         // c64 addr scrn border color
+        .const BACKGROUND_COLOR_ADDR = $D021     // c64 addr bkgrd color
+        .const CLEAR_SCREEN_KERNAL_ADDR = $E544  // addr Kernal clear screen
 
+        // Assembler variables for loops. 
+        // Total iterations will be _inner_max * _outer_max
+        .const INNER_MAX = $FF        // number of iterations of inner loop
+        .const OUTER_MAX = $B0        // number of iterations of outer loop
+       
 
-CrazyBorder:
+        // call kernal clear screen routine leave cursor upper left
+        jsr CLEAR_SCREEN_KERNAL_ADDR
+
 CrazyBorderLoop:
 
-// Total iterations will be inner_max * outer_max
-.var _inner_max = $FF        // number of iterations of inner loop
-.var _outer_max = $B0        // number of iterations of outer loop
-
-        // go to next boarder and background color
-        inc border_color_addr      // inc val at border color addr
-        inc background_color_addr  // inc val at bkgrd color addr
+        // next boarder and background color
+        inc BORDER_COLOR_ADDR      // inc val at border color addr
+        inc BACKGROUND_COLOR_ADDR  // inc val at bkgrd color addr
 
         // inc inner counter and if hasn't reached max then 
         // back to top of loop
         inc inner_counter          
-        lda #_inner_max
+        lda #INNER_MAX
         cmp inner_counter
         bne CrazyBorderLoop 
 
@@ -61,25 +56,13 @@ CrazyBorderLoop:
         // now inc and check outer loop counter
         // if we've completed all the outer loops then done
         inc outer_counter
-        lda #_outer_max
+        lda #OUTER_MAX
         cmp outer_counter
         beq Done
 
         // still more to do, back to top of inner loop
         jmp CrazyBorderLoop
 Done:
-        rts
-
-
-
-
-HelloStr: .text @"PRINTING HELLO 2\$00"    // string to print
-
-
-PrintHello:
-        lda #<HelloStr        // lsB of addr of string to print to A
-        ldy #>HelloStr        // msB of addr of str to print to Y
-        jsr PRINT_STRING_BASIC  // call kernal routine to print the string
         rts
 
 
