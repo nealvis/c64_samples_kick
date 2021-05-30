3db21aad2c798820c34dc43ee4f79d3ecbf705ef
Neal Smith <nealvis@gmail.com>
2021-05-30 15:46:01 -0700

    added comments and rearanged code.  also setting to multi color or high res based on last byte in sprite data now.

--- sprites_display/spritesdisplay.asm
+++ sprites_display/spritesdisplay.asm
@@ -24,14 +24,14 @@
         .const CLEAR_SCREEN_KERNAL_ADDR = $E544     // Kernal routine to clear screen
 
 
-// set the address for sprite 0 it must be evenly divisible by 64
-// sprite 0 / multicolor / color: $01
+// set the address for our sprite, sprite_0 aka sprite_ship.  It must be evenly divisible by 64
 *=$0900 "Sprite 0"
 
         // Byte 64 of each sprite contains the following:
         //   high nibble: high bit set (8) if multi color, or cleared (0) if single color/high res
         //   low nibble: this sprite's color in it 0-F
         sprite_0:
+        sprite_ship:
         .byte $00,$00,$00,$00,$00,$00,$00,$00
         .byte $00,$40,$00,$00,$13,$c0,$00,$5e
         .byte $b0,$00,$5e,$ac,$00,$12,$ab,$00
@@ -67,30 +67,63 @@
         // the hi 4 bits don't seem to be writable
         .const SPRITE_0_COLOR_REG_ADDR = $d027
 
+
+        ////////////////////////////////////////////////////////////////////////////
         // clear screeen leave cursor upper left
         jsr CLEAR_SCREEN_KERNAL_ADDR 
         
+        ////////////////////////////////////////////////////////////////////////////
+        // setup the system for our sprite, sprite_0 aka sprite_ship
+        // the steps are:
+        // Step 1. set the sprite mode for the sprite to multi color or high res (one color)
+        // Step 2. set the sprite data pointer for to the 64 bytes at sprite_ship label
+        // Step 3. set the distinct color for sprite 3 
+        // Step 4. set the global multi color sprite colors
+        // Step 5. Enable the sprite
+        // Step 6. Set sprite's location
 
+        ////// Step 1. //////
+
+        // assume single color (high res) and override below if needed
+        lda #$00
+        sta SPRITE_MODE_REG_ADDR
+
+        lda #$F0                // load mask in A 
+        bit sprite_0 + 63       // set Zero flag if the masked bits are all 0s
+                                // if any masked bits in the sprite + 63 were set then
+                                // assume its a multi colored sprite
+        beq skip_multicolor     // if Zero is set, ie no masked bits were set, then branch
+                                // to skip multi color mode.
         // set sprite 0 to muli color mode and all other sprites to high res
         lda #$01
         sta SPRITE_MODE_REG_ADDR
+skip_multicolor:
+        ////// Step 1 done //////
 
+        ////// Step 2 //////
         lda #(sprite_0 / 64)
         sta SPRITE_0_DATA_PTR_ADDR
+        ////// step 2 done //////
 
+        ////// step 3 //////
+        // set this sprite's color
+        lda sprite_0 + 63
+        sta SPRITE_0_COLOR_REG_ADDR
+        ////// step 3 done //////
+
+        ////// step 4 //////
         lda #$0d // multicolor sprites global color 1
         sta SPRITE_COLOR_1_ADDR
         lda #$01 // multicolor sprites global color 2
         sta SPRITE_COLOR_2_ADDR
+        ////// step 4 done //////
 
-        // set this sprite's color
-        lda sprite_0 + 63
-        sta SPRITE_0_COLOR_REG_ADDR
+        ////// step 5 //////
+        lda #$01                        // set the bit for sprite 0, note we are clearing other bits
+        sta SPRITE_ENABLE_REG_ADDR      // store to sprite enable register one bit for each sprite.
+        ////// step 5 done //////
 
-        // turn on sprit 0?
-        lda #$01
-        sta SPRITE_ENABLE_REG_ADDR
-
+        ////// step 6 //////
         // set sprite 0 x loc
         lda #22
         sta SPRITE_0_X_ADDR
@@ -98,6 +131,7 @@
         // set sprite 0 y loc
         lda #50
         sta SPRITE_0_Y_ADDR
-
+        ////// step 6 done //////
+        
         rts                     // program done, return
 
