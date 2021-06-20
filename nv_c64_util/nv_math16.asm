1c60eab5b8623da315a01a9e18ea8d71edecc333
Neal Smith <nealvis@gmail.com>
2021-06-20 15:19:09 -0700

    adc16_8signed and adc16_8unsigned for now adc16_8 maps to the unsigned version as that was how it was originally written

--- nv_c64_util/nv_math16.asm
+++ nv_c64_util/nv_math16.asm
@@ -1,5 +1,6 @@
 #importonce
 
+#import "nv_util_data.asm"
 
 //////////////////////////////////////////////////////////////////////////////
 // inline macro to add two 16 bit values and store the result in another
@@ -27,7 +28,27 @@
 //   addr16 is the address of the low byte of 16 bit operand
 //   addr8 is the address of the 8 bit operand
 //   result_addr is the address to store the result.
+.macro nv_adc16_8signed(addr16, addr8, result_addr)
+{
+    ldx #0
+    lda addr8
+    bpl Op2Positive
+    ldx #$ff
+Op2Positive:
+    stx scratch_byte
+    clc
+    adc addr16
+    sta result_addr
+    lda addr16+1
+    adc scratch_byte
+    sta result_addr+1
+}
+
 .macro nv_adc16_8(addr16, addr8, result_addr)
+{
+    nv_adc16_8unsigned(addr16, addr8, result_addr)
+}
+.macro nv_adc16_8unsigned(addr16, addr8, result_addr)
 {
     lda addr16
     clc
