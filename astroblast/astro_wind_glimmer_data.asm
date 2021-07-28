553e2cf8560eec9b4dfa8059707bda3402e19622
Neal Smith <nealvis@gmail.com>
2021-07-28 05:43:56 -0700

    moved one wind glimmer puff away from turret

--- astroblast/astro_wind_glimmer_data.asm
+++ astroblast/astro_wind_glimmer_data.asm
@@ -26,7 +26,7 @@ wind_step0_point_list_addr: .byte 37, 1     // x, y ie col, row
                             .byte 36, 5
                             .byte 39, 6
                             .byte 38, 7
-                            .byte 39, 9
+                            .byte 37, 9
                             .byte 36, 11
                             .byte 36, 12
                             .byte 35, 15
