0a42ca952039de2d07b437f06437ac9e88dca32c
Neal Smith <nealvis@gmail.com>
2021-08-29 10:44:26 -0700

    fixed the last commit which didn't update the hole sprite table as it should have

--- astroblast/astro_blackhole_data.asm
+++ astroblast/astro_blackhole_data.asm
@@ -15,11 +15,11 @@ hole_frame_counter: .byte 0
 hole_change_vel_at_x_loc: .byte 0
 
 hole_sprite_data_ptr_table:
-    .word sprite_explosion_0
-    .word sprite_explosion_1
-    .word sprite_explosion_2
-    .word sprite_explosion_3
-    .word sprite_explosion_4
+    .word sprite_hole_0
+    .word sprite_hole_1
+    .word sprite_hole_2
+    .word sprite_hole_3
+    .word sprite_hole_4
 
 /*
     .word sprite_hole_2
